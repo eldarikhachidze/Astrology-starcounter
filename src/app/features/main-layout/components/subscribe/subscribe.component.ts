841f82dbfeedcffeefc8fa92bf1e4644cace5aee
Administrator <xachidze.eldari@gmail.com>
2024-02-12 17:06:40 +0400

    fix: home page styles

--- src/app/features/main-layout/components/subscribe/subscribe.component.ts
+++ src/app/features/main-layout/components/subscribe/subscribe.component.ts
@@ -14,10 +14,33 @@ export class SubscribeComponent {
     active: new FormControl(true)
   });
 
+  isInputFocused: { [key: string]: boolean } = {};
+
   constructor(
     private router: Router,
     private subscribeService: SubscribeService
   ) {
+  }
+
+  isInputValid(controlName: string): boolean {
+    return !!this.form.get(controlName)?.valid;
+  }
+
+  onInputFocus(controlName: string) {
+    this.isInputFocused[controlName] = true;
+  }
+
+  onInputBlur(controlName: string) {
+    this.isInputFocused[controlName] = false;
+  }
+
+  isInvalidAndTouched(controlName: string): boolean {
+    const control = this.form.get(controlName);
+    return !!control && control.invalid && (control.touched || control.dirty);
+  }
+
+  get isInputFocusedFn(): (key: string) => boolean {
+    return (key: string) => this.isInputFocused[key];
   }
 
   submit() {
